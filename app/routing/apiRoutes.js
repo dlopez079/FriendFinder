bf8d8557b7cf27224f22b6e19c5cb429b23f8e51
dlopez079 <dlopez079@gmail.com>
2019-10-08 01:07:12 -0400

    Populated the javascript files, the html files, and the routes.

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,15 @@
+//WE REQUIRE THE ARRAY IN THE FRIENDS.JS FILE.
+var friends = require("../data/friends");
+
+//USE MODULE EXPORT TO MAKE CODE AVAILABLE TO OTHER FILES. 
+module.exports = function(app) {
+    
+    app.get("/api/friends", function(req, res){
+        res.json(friends)
+    });
+
+    app.post("/api/friends", function(req, res){
+        //CREATE YOUR LOG FOR COMPAIRING FRIENDS SCORES FROM SURVEY
+    });
+
+}
---
+++ server.js
@@ -0,0 +1,28 @@
+//DEPENDENCIES***************************************************
+var express = require("express");
+//***************************************************************
+
+
+//SET UP THE EXPRESS APP*****************************************
+var app = express();
+var PORT = 3000;
+//***************************************************************
+
+
+// SET UP THE EXPRESS APP TO HANDLE DATA PARSING*****************
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+// **************************************************************
+
+
+//REQUIRE ROUTING************************************************
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+// **************************************************************
+
+
+//APP LISTEN*****************************************************
+app.listen(PORT, function() {
+    console.log("Port at : " + PORT);
+})
